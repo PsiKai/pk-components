9047ea4954c24dcbfc59287b9511fed513f6d0bd
David Irvin <davidirvin47@gmail.com>
2024-09-14 13:13:08 -0600

    Setup basic local development

--- src/AppDev.tsx
+++ src/AppDev.tsx
@@ -1,12 +1,26 @@
-import { StrictMode } from "react"
+import React, { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import Button from "./components/Button"
+import "./index.css"
+
+// eslint-disable-next-line react-refresh/only-export-components
+const AppDev = () => {
+  const handleClick = (e: React.MouseEvent) => {
+    console.log("Button clicked", e)
+  }
+
+  return (
+    <>
+      <h1>Hello, world!</h1>
+      <Button onClick={handleClick}>Click me</Button>
+    </>
+  )
+}
 
 const root = createRoot(document.getElementById("root")!)
 
 root.render(
   <StrictMode>
-    <div>Hello, world!</div>
-    <Button />
+    <AppDev />
   </StrictMode>,
 )
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,10 +2,11 @@ import React from "react"
 
 type ButtonProps = {
   children?: React.ReactNode
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 
 function Button(props: ButtonProps) {
-  return <button>{props.children}</button>
+  return <button onClick={props.onClick}>{props.children}</button>
 }
 
 export default Button
