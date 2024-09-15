85ab024eeda6e48b84d44f856c285346c47335b9
David Irvin <63161310+PsiKai@users.noreply.github.com>
2024-09-15 02:01:39 -0600

    Import CSS files in index entrypoint (#5)
    * Import CSS files in index entrypoint
    * Pump patch version of package

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,4 @@
 export { Button } from "./components/Button"
+import "./components/Button/Button.css"
 export { LoadingSpinner } from "./components/LoadingSpinner"
+import "./components/LoadingSpinner/LoadingSpinner.css"
