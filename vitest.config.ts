39cfac895951dc640d3593000a6de564062fc940
David Irvin <davidirvin47@gmail.com>
2024-09-22 15:37:15 -0600

    Update vitest config for callback notation

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,20 @@
 import { defineConfig, mergeConfig } from "vitest/config"
 import viteConfig from "./vite.config"
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      globals: true,
-      environment: "jsdom",
-      setupFiles: "./tests/setup.ts",
-      coverage: {
-        provider: "istanbul",
-        include: ["src/**/*", "utils/**/*"],
-        exclude: ["src/dev/**/*"],
+export default defineConfig(configEnv => {
+  return mergeConfig(
+    viteConfig(configEnv),
+    defineConfig({
+      test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./tests/setup.ts",
+        coverage: {
+          provider: "istanbul",
+          include: ["src/**/*", "utils/**/*"],
+          exclude: ["src/dev/**/*"],
+        },
       },
-    },
-  }),
-)
+    }),
+  )
+})
