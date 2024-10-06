cb7d863bf25602aabb14ed25515fbb35b2b27c96
David Irvin <davidirvin47@gmail.com>
2024-10-06 00:36:23 -0600

    Implement Dropzone into FileInput component

--- src/lib/components/FileInput/FileInput.model.ts
+++ src/lib/components/FileInput/FileInput.model.ts
@@ -1,7 +1,17 @@
 import { AllHtmlAttributes } from "../../core-types"
 
-export type TFileInputProps = AllHtmlAttributes & {
+type TAllOptionalFileInputProps = AllHtmlAttributes & {
   id: string
   onChange: React.ChangeEventHandler<HTMLInputElement>
   fileDisplay?: "list" | "preview"
+  dropzone?: boolean
+  ref?: React.RefObject<HTMLInputElement>
 }
+
+type TDependentFileInputProps<T> = T extends { dropzone: boolean }
+  ? T & {
+      ref: React.RefObject<HTMLInputElement>
+    }
+  : T
+
+export type TFileInputProps = TDependentFileInputProps<TAllOptionalFileInputProps>
