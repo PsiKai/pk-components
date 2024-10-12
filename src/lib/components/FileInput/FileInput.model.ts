24ddda0f247f7ffcbd558761a5a2734274be342b
David Irvin <davidirvin47@gmail.com>
2024-10-12 15:05:41 -0600

    Explicitly set element type and label on FileInput

--- src/lib/components/FileInput/FileInput.model.ts
+++ src/lib/components/FileInput/FileInput.model.ts
@@ -1,7 +1,8 @@
 import { AllHtmlAttributes } from "../../core-types"
 
-export type TFileInputProps = AllHtmlAttributes & {
+export type TFileInputProps = AllHtmlAttributes<HTMLInputElement> & {
   id: string
+  label: string
   onChange: React.ChangeEventHandler<HTMLInputElement>
   fileDisplay?: "list" | "preview"
   dropzone?: boolean
