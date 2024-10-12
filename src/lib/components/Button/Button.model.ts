5a5f0f39c6c37efb01c89c013a43ab94f6291f21
David Irvin <davidirvin47@gmail.com>
2024-10-12 11:12:28 -0600

    Remove redundant props from Button

--- src/lib/components/Button/Button.model.ts
+++ src/lib/components/Button/Button.model.ts
@@ -1,15 +1,9 @@
-import React from "react"
 import { AllHtmlAttributes } from "../../core-types"
 
 export type TButtonProps = AllHtmlAttributes<HTMLButtonElement | HTMLAnchorElement> & {
-  children?: React.ReactNode
-  onClick?: React.MouseEventHandler<HTMLButtonElement>
-  className?: string
-  disabled?: boolean
   type?: "button" | "submit" | "reset"
   variant?: "primary" | "secondary" | "success" | "warning" | "danger"
   fit?: "small" | "medium" | "large" | "block" | "link"
   fill?: "solid" | "outline"
   pending?: boolean
-  href?: string
 }
