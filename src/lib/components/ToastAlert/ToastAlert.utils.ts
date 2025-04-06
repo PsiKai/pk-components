c6febba4cfab80e28a1c2261ff95257c19fff1d6
David Irvin <davidirvin47@gmail.com>
2025-04-06 11:29:52 -0600

    Fix toast bg color, position, and zindex

--- src/lib/components/ToastAlert/ToastAlert.utils.ts
+++ src/lib/components/ToastAlert/ToastAlert.utils.ts
@@ -25,9 +25,11 @@ export const ICON_MAP = (intent: TAlertIntent): ReactNode => {
 }
 
 export const toastStyle = (intent: TAlertIntent, origin: TToastOrigin): IToastAlertSyle => {
+  const { backgroundColor } = window.getComputedStyle(document.documentElement)
   return {
     "--alert-intent": `var(--${intent})`,
     "--alert-origin": originCalculation(origin),
+    backgroundColor,
   }
 }
 
